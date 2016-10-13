83c9cec2f115e41437f5de221d340fcd3dc2ba41
Brent Watson <brentmwatson@gmail.com>
2016-10-12 23:12:17 -0400

    No errors, but no outputs

--- game.rb
+++ game.rb
@@ -1,22 +1,22 @@
-require_relative "deck"  # => true
-include Comparable       # => Object
+require_relative "deck"
+include Comparable
 
 class Game
-  attr_accessor :player1_deck,  # => :player1_deck
-                :player2_deck,  # => :player2_deck
-                :play_war       # => nil
+  attr_accessor :player1_deck,
+                :player2_deck,
+                :play_war
 
   def initialize
     self.player1_deck = Deck.new
     self.player2_deck = Deck.new
-  end                             # => :initialize
+  end
 
   def play_war
     promt
     play_round
-    decide_winner
-    ask_for_rematch
-  end                # => :play_war
+    # decide_winner
+    # ask_for_rematch
+  end
 
   def play_round
     if self.player1_deck.cards.value > self.player1_deck.cards.value
@@ -28,7 +28,7 @@ class Game
     end
     self.player1_deck.shift
     self.player2_deck.shift
-  end                                     # => :play_round
+  end
 
 
   def total_round_outcome (round_winner)
@@ -39,16 +39,16 @@ class Game
     else
       round_winner.reduce(:+)
     end
-  end                                     # => :total_round_outcome
+  end
 
-  # def overall_winner
-  #   if total("player1") > total ("player2")
-  #     "player1"
-  #   else
-  #     "player2"
-  #   end
-  #
-  # end
+  def overall_winner
+    if total("player1") > total("player2")
+      "player1"
+    else
+      "player2"
+    end
+
+  end
   # player 1 winning
   # player 2 winnings
   # of wars
@@ -63,4 +63,4 @@ class Game
 # player 2 deck shift
 
 
-end  # => :total_round_outcome
+end
